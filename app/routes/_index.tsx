a4ec4b801eb886613b40f17a5cdd9ef2d4b71f53
yash raj <yashraj0207@outlook.com>
2024-09-15 17:31:52 +0530

    add prettier & import order rule in esilnt

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node"
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Reciprocal" },
-    { name: "description", content: "Welcome to Reciprocal!" },
-  ];
-};
+  return [{ title: "Reciprocal" }, { name: "description", content: "Welcome to Reciprocal!" }]
+}
 
 export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <h1 className="text-3xl font-semibold text-gray-800">Welcome to Reciprocal</h1>
     </div>
-  );
+  )
 }
-
